095290ab4adf614443a5b9a3223cf4d29ac8671b
allaintkj <tom.allain@protonmail.com>
2021-05-03 17:14:18 -0300

    scaffold initial design;

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-post-list',
+  selector: 'post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.scss']
 })
