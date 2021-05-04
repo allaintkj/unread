b0913431323e88b2dbcd85b945fad5fd652a9afb
allaintkj <tom.allain@protonmail.com>
2021-05-04 11:10:12 -0300

    setup routing; post fetching based on current route (top/new/ask/show/jobs) and page number;

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { PostService } from '../../services/post.service';
 
@@ -13,9 +14,24 @@ export class PostListComponent implements OnInit {
   public clrLight = 'light';
   public clrSec = 'secondary';
 
-  constructor(public postService: PostService) { }
+  constructor(
+    private router: Router,
+    public postService: PostService
+  ) { }
 
   ngOnInit(): void {
-    this.postService.getPostsHackerNews();
+    // Get the path to determine type of story we want
+    // top/new/ask/show/jobs
+    // If it's 'jobs' we need to substring, as the correct endpoint is 'jobstories'
+    let path = this.router.url.split('/')[1];
+    path = (path === 'jobs' ? path.substr(0, path.length - 1) : path);
+
+    // Determine page number to return posts for
+    let page = this.router.url.split('/')[2];
+    page = (parseInt(page) >= 1 ? page : '1');
+
+    // Have the service fetch our posts and update array for rendering
+    // Could probably use an observable to return the array to this component
+    this.postService.getPostsHackerNews(path, parseInt(page));
   }
 }
--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -14,11 +14,15 @@ export class PostService {
   constructor(private http: HttpClient) { }
 
   public getPostsHackerNews(storyType: string = 'top', pageNumber: number = 1): void {
+    // Get the full list of posts of <type>
     this.http.get<number[]>(`${this.apiHackerNews}${storyType}stories.json`).subscribe(
       posts => {
+        // Traverse the list according to page number
         posts.slice((pageNumber - 1) * this.postsPerPage, pageNumber * this.postsPerPage).forEach(id => {
+          // Fetch each post for this page number
           this.http.get<HNPost>(`${this.apiHackerNews}item/${id}.json`).subscribe(
             post => {
+              // Add to array for rendering
               this.postsHackerNews.push(post);
             }, err => {
               console.log(err);
